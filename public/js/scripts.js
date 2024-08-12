0cfc003872517c79915594552167dfd1430bc458
Ignacio Oteiza <oteizanacho@gmail.com>
2024-08-12 12:06:11 -0300

    links

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,39 +1,19 @@
 const universidades = document.getElementById('unibtn')
 const profesores = document.getElementById('profebtn')
 const materias = document.getElementById('matebtn')
+const topuni = document.getElementById('')
+const topprofe = document.getElementById('')
+
 
 universidades.addEventListener('click', (e) => {
-    const div = document.getElementById('link-container')
-    div.innerHTML = ''
-    console.log('universidades')
-    for(let i = 0; i < 5; i ++){
-        const link = document.createElement('a')
-        link.href = ""
-        link.innerText = 'UNLP'
-        div.append(link)
-    }
+    const universidades = document.getElementById('universidades')
+    universidades.style.display = 'block'
 })
 
 profesores.addEventListener('click', (e) => {
-    const div = document.getElementById('link-container')
-    div.innerHTML = ''
-    console.log('profesores')
-    for(let i = 0; i < 5; i ++){
-        const link = document.createElement('a')
-        link.href = ""
-        link.innerText = 'profesor'
-        div.append(link)
-    }
+    
 })
 
 materias.addEventListener('click', (e) => {
-    const div = document.getElementById('link-container')
-    div.innerHTML = ''
-    console.log('materias')
-    for(let i = 0; i < 5; i ++){
-        const link = document.createElement('a')
-        link.href = ""
-        link.innerText = 'Analisis matematico 1'
-        div.append(link)
-    }
+    
 })
